1cb712a909114421227ce2d3dee224d5ca557c3d
Caleb <caleb@snapcracklecode.com>
2024-04-05 14:58:17 -0400

    added ios string ffi

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,31 @@
+extern crate libc;
+
+mod string;
+
+use string::StringPtr;
+
+#[no_mangle]
+pub unsafe extern fn rust_string_ptr(s: *mut String) -> *mut StringPtr {
+  Box::into_raw(Box::new(StringPtr::from(&**s)))
+}
+
+#[no_mangle]
+pub unsafe extern fn rust_string_destroy(s: *mut String) {
+  let _ = Box::from_raw(s);
+}
+
+#[no_mangle]
+pub unsafe extern fn rust_string_ptr_destroy(s: *mut StringPtr) {
+  let _ = Box::from_raw(s);
+}
+
+#[no_mangle]
+pub unsafe extern fn hello_world(name: *mut StringPtr) -> *mut String {
+  let name = (*name).as_str();
+  let response = format!("Hello {}!", name);
+  Box::into_raw(Box::new(response))
+}
+
 #[cfg(feature = "jni")]
 #[allow(non_snake_case)]
 pub mod android {
